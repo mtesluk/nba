e39bcede12a46d916ab830d2ca98fa6b4d7431f4
mtesluk <mateusz@tesluk.pl>
2019-10-26 17:23:09 +0200

    Update sidebar

--- nba-stats/src/app/navigation/sidenav/sidenav.component.ts
+++ nba-stats/src/app/navigation/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { MatDialog } from '@angular/material';
+import { LoginDialogComponent } from 'src/app/login/login.component';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,14 +11,39 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   @Output() sidenavClose = new EventEmitter();
+  get user() {
+    return this._auth.getUserUsername();
+  }
+  get user_credits() {
+    return this._auth.getUserCredits();
+  }
  
-  constructor() { }
+  constructor(
+    private _auth: AuthService,
+    public dialog: MatDialog,
+  ) { }
  
   ngOnInit() {
   }
  
   onSidenavClose = () => {
     this.sidenavClose.emit();
+  }
+
+  isAuthenticated() {
+    return this._auth.isAuthenticated();
+  }
+
+  isAdmin() {
+    return this._auth.isAdmin();
+  }
+
+  logout() {
+    return this._auth.logout();
+  }
+
+  openDialog(): void {
+    this.dialog.open(LoginDialogComponent, {});
   }
 
 }
