2edf4b539d3cce851d3ae27bcc6106cce91e789c
mtesluk <mateusz@tesluk.pl>
2019-12-13 22:02:16 +0100

    dadas

--- project/nba/views/team.py
+++ project/nba/views/team.py
@@ -1,4 +1,5 @@
 import json
+import os
 from rest_framework import viewsets, serializers, decorators, response
 from rest_framework.permissions import AllowAny
 from django_filters import rest_framework as filters
@@ -40,7 +41,8 @@ class TeamViewSet(CommonMixin, viewsets.ModelViewSet):
 
     @decorators.list_route(methods=['get'], permission_classes=[AllowAny, ],)
     def coordinates(self, request):
-        with open('nba/static/cities.json', 'r') as file:
+        print(os.getcwd())
+        with open('nba/views/cities.json', 'r') as file:
             try:
                 cities = [{**city, 'id': self.queryset.get(name=city['name']).id} for city in json.load(file)]
             except Exception:
